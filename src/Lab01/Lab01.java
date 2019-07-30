e0c67ece88404d1ee77b915681a065f5beb319d0
afahrer <afahrer2112@gmail.com>
2019-07-30 12:33:34 -0400

    Laab 1: changed output

--- src/Lab01/Lab01.java
+++ src/Lab01/Lab01.java
@@ -4,18 +4,23 @@ import java.util.Arrays;
 
 public class Lab01 {
     public static void main(String[] args) {
-        int[] list1 = {4, 7, 3, 2, 8, 1, 5};
+        int[] list1 = {4, 7, 3, 2, 8, 1, 6};
         int[] list2 = {1, 2, 3, 1, 2, 3, 3, 3};
         int[] list3 = {1, 2, 3, 1, 2, 3, 6, 3, 6, 5, 4, 3};
-        int[] list4 = {6, 5, 4, 3};
-        System.out.println(partitionable2to1(list1, new int[list1.length], 0));
-        System.out.println(partitionable(list2, new int[list2.length], 0));
-        System.out.println(partitionable(list3, new int[list3.length], 0));
-        System.out.println(partitionable(list4, new int[list4.length], 0));
+        int[] list4 = {6, 5, 4, 2};
+        int[] answer;
+        System.out.println(partitionable2to1(list1, answer = new int[list1.length], 0));
         System.out.println(Arrays.toString(list1));
+        System.out.println(Arrays.toString(answer));
+        System.out.println(partitionable(list2, answer = new int[list2.length], 0));
         System.out.println(Arrays.toString(list2));
+        System.out.println(Arrays.toString(answer));
+        System.out.println(partitionable(list3, answer = new int[list3.length], 0));
         System.out.println(Arrays.toString(list3));
+        System.out.println(Arrays.toString(answer));
+        System.out.println(partitionable(list4, answer = new int[list4.length], 0));
         System.out.println(Arrays.toString(list4));
+        System.out.println(Arrays.toString(answer));
     }
 
     public static boolean partitionable(int[] list1, int[] list2, int dropIndex) {
