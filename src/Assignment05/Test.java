2ca81aa11d7d9ab6fbe393422c96fdbdbdcc2623
adam <afahrer2112@gmail.com>
2019-08-19 16:33:27 -0400

    Assignment 5: hashmap test complete

--- src/Assignment05/Test.java
+++ src/Assignment05/Test.java
@@ -2,11 +2,35 @@ package Assignment05;
 
 public class Test {
     public static void main(String[] args) {
-        Hashmap hashmap = new Hashmap(0);
-        hashmap.insert(new Employee(1));
-        hashmap.insert(new Employee(2));
-        hashmap.insert(new Employee(402));
-        hashmap.delete(402);
-        hashmap.printHash();
+        System.out.println("Linear\n");
+        buildHash(0);
+        System.out.println("Quadratic\n");
+        buildHash(1);
+        System.out.println("Double Hash\n");
+        buildHash(2);
+    }
+    private static void buildHash(int mode) {
+        Hashmap hash = new Hashmap(mode);
+        Employee[] list = {
+                new Employee(1,"Adam","Fahrer", "Computer Science", 3),
+                new Employee(2,"Phil","Smith", "Computer Science", 3),
+                new Employee(402,"Amy","Smith", "Science", 7),
+                new Employee(802,"Bill","Walker", "Math", 1),
+                new Employee(3,"Jill","Doe", "English", 12),
+                new Employee(66,"Mike","Smith", "Math", 6),
+                new Employee(55,"John","Doe", "Music", 6),
+                new Employee(456,"Bob","Smith", "Art", 4),
+                new Employee(88,"Andy","Doe", "English", 22),
+                new Employee(1023,"Sarah","Smith", "English", 14),
+        };
+        for (Employee employee: list) {
+            hash.insert(employee);
+        }
+        hash.delete(402);
+        hash.delete(88);
+        System.out.println("Retrieve 3\n" + hash.retrieve(3));
+        System.out.println();
+        hash.printHash();
+        System.out.println();
     }
 }
